0b8b662a6d0235b3b7bd3afb4bc0bcadd4d61a6d
Roi Zerah <roizerah@Rois-MacBook-Pro.local>
2024-11-07 14:27:31 +0200

    first try for initial check

--- FVCalc.java
+++ FVCalc.java
@@ -1,6 +1,15 @@
 // Computes the future value of a saving investment.
+
+import java.util.Scanner;
+
 public class FVCalc {
-	public static void main(String[] args){
-		// Replace this comment with your code
+	public static void main(String[] args)
+	{
+		int currentValue = Integer.parseInt(args[0]);
+		double rate = Double.parseDouble(args[1]);
+		double trurate = (rate)/100;
+		int years = Integer.parseInt(args[2]);
+		double futureValue = currentValue * Math.pow(1.0 + trurate ,years);
+		System.out.println("After " + years + " years, " +"$" + currentValue + " saved at " + rate + "% will yield $" + (int)futureValue);
 	}
 }
--- NumWords.java
+++ NumWords.java
@@ -1,6 +1,14 @@
 // Prints a given number using a hundreds, tens, and units notation.
+
+import java.util.Scanner;
+
 public class NumWords {
-	public static void main(String args[]) {
-	    // Replace this comment with your code
+	public static void main(String args[])
+	{
+		int completenum = Integer.parseInt(args[0]);
+		int hundrednum = completenum/100;
+		int tennum = (completenum - (hundrednum*100))/10;
+		int onenum = (completenum - (hundrednum*100) - (tennum*10));
+		System.out.println(hundrednum + " hundreds, " + tennum + " tens, and " + onenum + " ones.");
 	}
 }
